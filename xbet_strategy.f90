81ed1ed38f21c8dbf239e7f1b8b32251c8e1c701
Beliavsky <beliavsky@aol.com>
2025-05-19 16:47:32 -0400

    add vary_frac toggle allow bet size to increase when few bets remain

--- xbet_strategy.f90
+++ xbet_strategy.f90
@@ -1,15 +1,19 @@
 program xbet_strategy
-   use kind_mod, only: dp
-   use bet_mod, only: bet, tol
+   ! simulate betting a fraction of wealth
+   use kind_mod  , only: dp
+   use bet_mod   , only: bet, tol
+   use random_mod, only: random_seed_init
    implicit none
    real(kind=dp), parameter :: prob = 0.60_dp, gain = 1.0_dp, loss = -1.0_dp, &
                                wealth_init = 25.0_dp, wealth_max = 250.0_dp, &
                                frac_min = 0.05_dp, frac_h = 0.05_dp
    integer, parameter :: max_bets = 300, npaths = 10**5, nfrac = 20
    integer :: i, ierr, ifrac, ipath, nbets(npaths)
-   real(kind=dp) :: wealth, bet_size, frac, wealth_term(npaths)
+   real(kind=dp) :: wealth, bet_size, frac, wealth_term(npaths), frac_use
    logical, parameter :: verbose = .false.
+   logical, parameter :: vary_frac = .true. ! allow bet size to increase when few bets remain?
    character (len=*), parameter :: fmt_cr = "(a20,':',*(1x,f8.4))", fmt_ci = "(a20,':',*(1x,i8))"
+   call random_seed_init(123)
    print fmt_cr,"initial wealth", wealth_init
    print fmt_cr,"maximum wealth", wealth_max
    print fmt_cr,"prob gain", prob
@@ -17,6 +21,7 @@ program xbet_strategy
    print fmt_cr,"loss", loss
    print fmt_ci,"maximum # of bets", max_bets
    print fmt_ci,"# of paths", npaths
+   print "(a20,':',*(1x,l1))", "vary_frac", vary_frac
    print "(/,'stats on terminal wealth vs. fractional bet size')"
    print "(*(a12))", "frac_bet", "mean", "min", "max", "prob_max", "mean_#bets"
    do ifrac = 1, nfrac
@@ -26,7 +31,12 @@ program xbet_strategy
          wealth = wealth_init
          do i = 1, max_bets
             if (verbose) write (*, "(/,'bet #', i0, /, 'wealth = ',f0.1)") i, wealth
-            bet_size = min(frac*wealth, wealth_max - wealth)
+            if (vary_frac) then
+               frac_use = min(1.0_dp, max(frac, (wealth_max/wealth)**(1.0_dp/(max_bets-i)) - 1.0_dp))
+            else
+               frac_use = frac
+            end if
+            bet_size = min(frac_use*wealth, wealth_max - wealth)
             call bet(bet_size, prob, gain, loss, wealth_max, wealth, ierr)
             if (wealth <= 0.0_dp) then
                if (verbose) print *, "lost all money"
